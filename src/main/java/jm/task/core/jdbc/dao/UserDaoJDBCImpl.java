de79f5b2da6387cfbd1ecdadf65792b5083b2126
SvyatoslavKochergin <konenero@mail.ru>
2021-06-22 19:00:24 +0300

    14062021

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -22,60 +22,43 @@ public class UserDaoJDBCImpl implements UserDao {
                 + "lastName VARCHAR(10),"
                 + " age INT,"
                 + " PRIMARY KEY(id));";
-        try {
-            Connection con = Util.setConnection();
-            Statement st = con.createStatement();
+
+        try (Connection con = Util.setConnection(); Statement st = con.createStatement()) {
             int res = st.executeUpdate(myTableName);
-            con.close();
-            st.close();
         } catch (SQLException e) {
         }
     }
 
     public void dropUsersTable() {
-        try {
-            Connection con = Util.setConnection();
-            Statement st = con.createStatement();
+        try (Connection con = Util.setConnection(); Statement st = con.createStatement()) {
             int res = st.executeUpdate("drop table users");
-            con.close();
-            st.close();
         } catch (SQLException e) {
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
-        try {
-            Connection con = Util.setConnection();
-            Statement st = con.createStatement();
+        try (Connection con = Util.setConnection(); Statement st = con.createStatement()) {
             int res = st.executeUpdate(String.format("INSERT INTO users ("
-                    + "name, lastName, age) "
-                    + "VALUES ('%s', '%s', '%s');"
-                    ,name, lastName, age));
-            con.close();
-            st.close();
+                            + "name, lastName, age) "
+                            + "VALUES ('%s', '%s', '%s');"
+                    , name, lastName, age));
         } catch (SQLException e) {
-            e.printStackTrace();
         }
         System.out.println(String.format("User с именем %s добавлен в базу данных", name));
     }
 
     public void removeUserById(long id) {
-        try {
-            Connection con = Util.setConnection();
-            Statement st = con.prepareStatement(String
-                    .format("delete from users where id = %s", id));
+        try (Connection con = Util.setConnection(); Statement st = con.prepareStatement(String
+                .format("delete from users where id = %s", id));) {
         } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 
     public List<User> getAllUsers() {
         List<User> listUsers = new ArrayList<>();
-        try {
-            Connection con = Util.setConnection();
+        try (Connection con = Util.setConnection();
             Statement st = con.createStatement();
-            ResultSet res = st.executeQuery("SELECT * FROM users");
+            ResultSet res = st.executeQuery("SELECT * FROM users")){;
             while (res.next()) {
                 User user = new User(res.getString("name"),
                         res.getString("lastName"),
@@ -89,12 +72,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try {
-            Connection con = Util.setConnection();
-            Statement st = con.createStatement();
+        try (Connection con = Util.setConnection(); Statement st = con.createStatement()) {
             int res = st.executeUpdate("truncate table users");
         } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
